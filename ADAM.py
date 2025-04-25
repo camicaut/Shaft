042798dcc7279343cf6f582c56efb4b1683d29c8
camicaut <syahmitoogoi@gmail.com>
2025-04-25 10:48:53 +0800

    Update Shaft.py

--- ADAM.py
+++ ADAM.py
@@ -9,7 +9,7 @@ import os
 from glob import glob
 import matplotlib.pyplot as plt
 
-st.header("Advanced corrodeD pipe structurAl integrity systeM (ADAM)")
+st.header("Shaft Fracture")
 
 st.subheader('Dimensional Parameters')
 htp="https://www.researchgate.net/profile/Changqing-Gong/publication/313456917/figure/fig1/AS:573308992266241@1513698923813/Schematic-illustration-of-the-geometry-of-a-typical-corrosion-defect.png"
